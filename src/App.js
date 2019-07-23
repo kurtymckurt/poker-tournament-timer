6021aa436ded5d7263cfa5c38587a60e4c0182ea
Kurt Lehrke <kurtymckurt@gmail.com>
2019-07-22 23:26:41 -0500

    OK we got the configuration working now! Wooo!!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React  from 'react';
 import Reflux from 'reflux';
-import Settings  from './images/settings-512.png';
+import Settings  from './images/settings.svg';
 import Configuration  from './Configuration';
 import ConfigurationStore from './ConfigurationStore';
 import Timer from './Timer';
@@ -31,21 +31,21 @@ class App extends Reflux.Component {
     return (
       <div>
         <div className="container-fluid">
-          <div className="row">
+          <div className="row bg-dark text-light">
             <div className="h1 text-center col-md-11">
-              Poker Timer
+            {me.state.name}
             </div>
-            <div className="text-right col-md-1">
+            <div className="text-right col-md-1 pt-5">
               <a href="#" ><img height="30" width="30" alt="Settings image" src={Settings} onClick={this.settingsClick}></img></a>
             </div>
-
             <Configuration isPaneOpen={this.state.isConfigOpen}/>
-
-            <div>{me.state.name}</div>
-
-
-            <Timer />
           </div>
+          <div className="row">
+            <div className="col-md-12 h1 text-center">
+              <Timer />
+            </div>
+          </div> 
+          
         </div>   
       </div>
     );
--- src/Configuration.js
+++ src/Configuration.js
@@ -1,5 +1,7 @@
-import React  from 'react';
-// import Modal from 'react-modal';
+import React, { Component }  from 'react';
+import Modal from 'react-modal';
+import {render} from 'react-dom';
+import Form from "react-jsonschema-form";
 import SlidingPane  from 'react-sliding-pane';
 import 'react-sliding-pane/dist/react-sliding-pane.css';
 import ConfigurationActions from './ConfigurationActions';
@@ -12,17 +14,55 @@ export default class Configuration extends React.Component {
         super(props);
         this.state = {
             isPaneOpen: false,
-            config: ""
+            config: "",
+            configObject: {}
         }
         this.closePane = this.closePane.bind(this);
         this.updateConfiguration = this.updateConfiguration.bind(this);
         this.updateJson = this.updateJson.bind(this);
         this.readTextFile = this.readTextFile.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.schema = {
+            title: "Configuration Settings",
+            type: "object",
+            required: ["name", "blind_levels"],
+            properties: {
+              name: {type: "string", title: "Name", default: "Poker Tournament"},
+              initial_player_count: {type: "number", title: "Initial Player Count", default: 9},
+              blind_levels: {type: "number", title: "Blind Level Count", default: 30},
+              blind_time: {type: "number", title: "Blind Level Time", default: 15},
+              break_time: {type: "number", title: "Break Level Time", default: 5},
+              levels_between_break: {type: "number", title: "Levels Between Break", default: 3},
+              buyin: {type: "number", title: "Buyin Amount", default: 10},
+              rebuy: {type: "number", title: "Rebuy Amount", default: 10},
+              addon: {type: "number", title: "Addon Amount", default: 0},
+              blinds: {type: "array", title: "Blind Levels", items: {
+                 title : "Level",
+                 type: "object", 
+                  properties: {
+                    small_blind: {type: "number", title: "Small Blind"},
+                    big_blind: {type: "number", title: "Big Blind"},
+                    ante: {type: "number", title: "Ante"}     
+                  }     
+              }}
+            }
+          };
+
+        this.uiSchema = {
+            "blinds": {
+                "ui:options": {
+                    orderable: true,
+                    addable: true,
+                    removable: true
+                }
+            }
+        };
     }
 
-    // componentDidMount() {
-    //     Modal.setAppElement(this.el);
-    // }
+    componentDidMount() {
+        Modal.setAppElement(this.el);
+    }
 
     componentWillReceiveProps(nextProps) {
         // You don't have to do this check first, but it can help prevent an unneeded render
@@ -38,33 +78,51 @@ export default class Configuration extends React.Component {
     }
 
     updateJson(event) {
+        this.updateRawJson(event.target.value);
+    }
+
+    updateRawJson(rawJson) {
+        console.log(rawJson);
         this.setState({
             isPaneOpen: this.state.isPaneOpen,
-            config: event.target.value
+            config: rawJson,
+            configObject: JSON.parse(rawJson)
         })
+        ConfigurationActions.configChange(rawJson);
+    }
+
+    updateJsonObject(object) {
+        const rawJson = JSON.stringify(object);
+        console.log(object);
+        this.setState({
+            isPaneOpen: this.state.isPaneOpen,
+            config: rawJson,
+            configObject: object
+        });
+        ConfigurationActions.configChange(rawJson);
     }
 
     updateConfiguration() {
         ConfigurationActions.configChange(this.state.config);
-        this.closePane();
+        // this.closePane();
+    }  
+
+    readTextFile (file) {
+        var me = this;
+        console.log(file);
+        var reader = new FileReader()
+        reader.onload = function (event) {
+            me.updateRawJson(event.target.result);
+        }
+        reader.readAsText(file);
     }
 
-    readTextFile(fileObject) {    
-        readTextFile = file => {
-            var rawFile = new XMLHttpRequest();
-            rawFile.open("GET", file, false);
-            rawFile.onreadystatechange = () => {
-                if (rawFile.readyState === 4) {
-                    if (rawFile.status === 200 || rawFile.status == 0) {
-                        var allText = rawFile.responseText;
-                        this.setState({
-                            text: allText
-                        });
-                    }
-                }
-            };
-            rawFile.send(null);
-        };
+    log(type) {
+        console.log(type);
+    }
+
+    onSubmit(response) {
+        this.updateJsonObject(response.formData);
     }
 
     render() {
@@ -76,29 +134,36 @@ export default class Configuration extends React.Component {
             isOpen={ me.state.isPaneOpen }
             title='Configuration'
             subtitle=''
-            onRequestClose={this.closePane}>
+            onRequestClose={me.closePane}>
             <div className="container-fluid"> 
                 <div className="row">
-                    <div className="col-md-2">
+                    <div className="col-md-12 text-center">
+                        Configuration import/export
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-2 text-right">
                     <FilePicker
                         extensions={['json']}
-                        onChange={FileObject => ( this.readTextFile(FileObject) )}
+                        onChange={me.readTextFile}
                         onError={errMsg => ( console.log(errMsg) )}>  
                         <img src={ImportImg} height="30" width="30"></img>
                     </FilePicker>
                     </div>
                     <div className="col-md-10">
                         <form>
-                            <textarea className="form-control text-full-width" onChange={me.updateJson} />
+                            <textarea className="form-control text-full-width" value={me.state.config} onChange={me.updateJson} />
                         </form>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-12 text-center">
-                        <button name="button" onClick={me.updateConfiguration}>apply</button>
-                    </div>
-                </div>
-                {me.state.config}
+            </div>
+            <div className="container-fluid">
+                <Form schema={me.schema}
+                     uiSchema={me.uiSchema}
+                     formData={me.state.configObject}
+                     onChange={me.log}
+                     onSubmit={me.onSubmit}
+                     onError={me.log} />
             </div>
         </SlidingPane>
         );
